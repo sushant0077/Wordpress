0330a240412b0db8c17bb6a7380002a921b256d1
Sushant shewane <sushant0077@gmail.com>
2016-02-16 15:32:55 +0530

    updated some new filter and imp functions

--- wp_imp_filter.php
+++ wp_imp_filter.php
@@ -31,4 +31,104 @@ function remove_head_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'remove_head_scripts' );
 
+/**
+  * This Function for SearchFilter
+  * @param query result
+  * @return none
+  * @author Sushant Shewane
+ **/
+
+function SearchFilter($query) {
+  if ($query->is_search) {
+    $query->set('post_type', 'article');
+  }
+  return $query;
+}
+
+/**
+  * This Function Get Post ID by meta key and meta value
+  * @param query result
+  * @return none
+  * @author Sushant Shewane
+    Date 14-01-15
+ **/
+
+
+
+ function get_post_id_by_meta_key_and_value($key, $value) {
+  global $wpdb;
+  $meta = $wpdb->get_results("SELECT * FROM `".$wpdb->postmeta."` WHERE meta_key='".$wpdb->escape($key)."' AND meta_value='".$wpdb->escape($value)."'");
+  if (is_array($meta) && !empty($meta) && isset($meta[0])) {
+    $meta = $meta[0];
+    } 
+  if (is_object($meta)) {
+    return $meta->post_id;
+    }
+  else {
+    return false;
+    }
+ }
+ 
+ 
+ /**
+  * This Function to get the hierarchy category. i.e parent->child
+  * @param query result
+  * @return none
+  * @author Sushant Shewane
+ **/
+
+function get_cat_hierchy($parent,$args){
+
+  $cats = get_categories($args);
+  $ret = new stdClass;
+
+  foreach($cats as $cat){
+    if($cat->parent==$parent){
+      $id = $cat->cat_ID;
+      $ret->$id = $cat;
+      $ret->$id->children = get_cat_hierchy($id,$args);
+    }
+  }
+
+  return $ret;
+}
+
+$args = array(
+   //'type'                     => 'post',
+   'child_of'                 => 0,
+   'parent'                   => '',
+   'orderby'                  => 'name',
+   'order'                    => 'ASC',
+   'hide_empty'               => 0,
+   'hierarchical'             => 1,
+   'exclude'                  => '',
+   'include'                  => '',
+   'number'                   => '',
+   'taxonomy'                 => 'tax-article',
+   'pad_counts'               => false 
+
+ ); 
+
+ //$categories = get_categories( $args );
+
+ $Featured_prgs = get_cat_hierchy(0,$args);
+ 
+  /**
+  * This Function to get to get image Alt text.
+  * @param query result
+  * @return none
+  * @author Sushant Shewane
+ **/
+ 
+ function get_image_alt_text($post_thumb_id){
+
+  $thumb_id = get_post_thumbnail_id($post_thumb_id);
+  $alt = get_post_meta($thumb_id, '_wp_attachment_image_alt', true);
+  
+  if(count($alt))
+    return $alt;
+    
+}
+
+
 ?>
