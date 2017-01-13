2bb14108a3385395ed6eec967eee3464c16c79a4
Sushant shewane <sushant0077@gmail.com>
2017-01-13 11:48:06 +0530

    Update wp_script.js

--- wp_script.js
+++ wp_script.js
@@ -2,7 +2,7 @@
 * Wordpress Ajax Call variable
 * @sushant shewane
 * @ 13-01-17
-* @Description : Add this to your header.php file in Head tag and call AjaxCall varialbe when you are doing ajax functionlity
+* @Description : Add this to your header.php file in Head tag and call AjaxCall variable when you are doing ajax functionlity
 */ 
 var AjaxCall = "<?php echo admin_url('admin-ajax.php'); ?>";
 
